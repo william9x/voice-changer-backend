68c00bd48ec6976d46932a61011d0e513697d263
LiamH <lehoangnam197@gmail.com>
2024-03-12 14:22:37 +0700

    feat: Create VoiceModelFactory

--- voice_model_factory.py
+++ voice_model_factory.py
@@ -1,56 +1,56 @@
 from rvc.modules.vc.modules import VC
 
-MODEL_IDS = [
-    "trump",
-    "elon_musk",
-    "taylor_swift",
-    "obama",
-    "jung_kook",
-    "charlie_puth",
-    "doja_cat",
-    "justin_bieber",
-    "biden",
-    "wrld_juice",
-    "patrick_star",
-    "katy_perry",
-    "jay_z",
-    "dua_lipa",
-    "bts_jimin",
-    "homer_simpson",
-    "the_weeknd",
-    "lady_gaga",
-    "nicki_minaj",
-    "blackpink_jennie",
-    "redvelvet_irene",
-    "bad_bunny",
-    "elton_john",
-    "harry_styles",
-    "troye_sivan",
-    "britney_spears",
-    "bruno_mars",
-    "chris_brown",
-    "blackpink_jiso",
-    "rosalia",
-    "bilie_eilish",
-    "ariana_grande",
-    "olivia_rodrigo",
-    "rihanna",
-    "starrail_seele",
-    "snsd_taeyeon",
-    "bts_jhope",
-    "bts_suga",
-    "starrail_7th",
-    "bts_taehyung",
-    "jack_black",
-    "quasimoto",
-    "post_malone",
-    "21savage",
-    "kim_jong_un",
-    "putin",
-    "mickey_mouse",
-    "batman",
-    "messi",
-    "godzilla",
+MODEL_PATHS = [
+    "trump.pth",
+    "elon_musk.pth",
+    "taylor_swift.pth",
+    "obama.pth",
+    "jung_kook.pth",
+    "charlie_puth.pth",
+    "doja_cat.pth",
+    "justin_bieber.pth",
+    "biden.pth",
+    "wrld_juice.pth",
+    "patrick_star.pth",
+    "katy_perry.pth",
+    "jay_z.pth",
+    "dua_lipa.pth",
+    "bts_jimin.pth",
+    "homer_simpson.pth",
+    "the_weeknd.pth",
+    "lady_gaga.pth",
+    "nicki_minaj.pth",
+    "blackpink_jennie.pth",
+    "redvelvet_irene.pth",
+    "bad_bunny.pth",
+    "elton_john.pth",
+    "harry_styles.pth",
+    "troye_sivan.pth",
+    "britney_spears.pth",
+    "bruno_mars.pth",
+    "chris_brown.pth",
+    "blackpink_jiso.pth",
+    "rosalia.pth",
+    "bilie_eilish.pth",
+    "ariana_grande.pth",
+    "olivia_rodrigo.pth",
+    "rihanna.pth",
+    "starrail_seele.pth",
+    "snsd_taeyeon.pth",
+    "bts_jhope.pth",
+    "bts_suga.pth",
+    "starrail_7th.pth",
+    "bts_taehyung.pth",
+    "jack_black.pth",
+    "quasimoto.pth",
+    "post_malone.pth",
+    "21savage.pth",
+    "kim_jong_un.pth",
+    "putin.pth",
+    "mickey_mouse.pth",
+    "batman.pth",
+    "messi.pth",
+    "godzilla.pth",
 ]
 
 
@@ -58,14 +58,14 @@ class VoiceModelFactory:
     def __init__(self):
         self._models = {}
 
-        for model in MODEL_IDS:
-            self.init_models(model)
+        for path in MODEL_PATHS:
+            self.init_models(path)
 
-    def init_models(self, model_id):
-        print(f"Loading model ${model_id}")
+    def init_models(self, model_path):
+        print(f"Loading model ${model_path}")
         vc = VC()
-        self._models[model_id] = vc.get_vc(model_id)
+        self._models[model_path] = vc.get_vc(model_path)
 
-    def get_model(self, model_id):
-        print(f"Model selected: {model_id}")
-        return self._models.get(model_id)
+    def get_model(self, model_path):
+        print(f"Model selected: {model_path}")
+        return self._models.get(model_path)
